707049e0e63700ddc02ef6feed80a42141bec4cd
rakibtg <rakibtg@gmail.com>
2019-05-06 01:21:22 +0600

    some random thoughts

--- nebula.js
+++ nebula.js
@@ -6,7 +6,8 @@ const state = {
   counter: 0,
   users: {
     counter: 0,
-  }
+  },
+  languages: [ 'php', 'javascript', 'go', 'dart', 'python', 'java' ]
 }
 
 const $StateDomMap = {
@@ -16,6 +17,12 @@ const $StateDomMap = {
       'innerHTML',
       'attribute'
     ]
+  },
+  languages: {
+    type: 'map',
+    class: {
+
+    }
   }
 }
 
@@ -76,12 +83,43 @@ const handleClickReset = () => {
   })
 }
 
+/**
+ * Here we will write a component who is 
+ * responsible for rendering and updating a list from state.
+ */
+const COMPONENT_listOfLanguages = {
+  $mounted: false,
+  set: {
+    mounted: status => {
+      COMPONENT_listOfLanguages.$mounted = status
+    }
+  },
+  render: () => {
+    if(!COMPONENT_listOfLanguages.$mounted) {
+      // create the dom.
+      COMPONENT_listOfLanguages.set.mounted(true)
+      return `
+        <ul>
+          ${state.languages.map((lang, index) => {
+            return `<li>${lang}</li>`
+          }).join('')}
+        </ul>
+      `
+    } else {
+      // Update the dom.
+
+    }
+  }
+}
+
 template = `
   <div class='counter ${$StateDomMap.counter.class}'>${state.counter}</div>
   <div class='buttons'>
     <button onclick="handleClickIncrement()">+1</button>
     <button onclick="handleClickDecrement()">-1</button>
     <button onclick="handleClickReset()">Reset</button>
+    <hr/>
+    ${COMPONENT_listOfLanguages.render()}
   </div>
 `
 
